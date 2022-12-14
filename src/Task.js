240751986660ac758c97578909edea7bcf60f159
Nanwjjj <nanikwijaya1@gmail.com>
2022-12-14 19:00:45 +0700

    First Commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Nanik Wijayanti" // TODO: replace this
+const studentId = "FE2927701" // TODO: replace this
+
+export { url, studentName, studentId }
